f5d757dfec5865d5134a86dadf47454df416f086
Tyler Sell <tylerselldesign@gmail.com>
2019-05-23 14:59:49 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,23 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get "/figures" do 
+    @firgures = Figure.all
+    erb :"figures/index"
+  end
+  
+  get "/figures/new" do 
+    @titles = Title.all 
+    @landmarks = Landmark.all 
+    erb :"figures/new"
+  end
+  
+  get "/figures/:id" do 
+    @figure = Figure.find(params[:id])
+    erb :"figures/show"
+  end
+  
+  get "/figures/:id/edit" do 
+    
+  end
+    
 end
